6a57780777f58d8ce49a3cffdee73858ac347a4a
Mirko Lenz <mirko@mirkolenz.com>
2025-01-30 10:10:44 +0100

    build: update devShell

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
         }:
         {
           devShells.default = pkgs.mkShell {
+            LD_LIBRARY_PATH = lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.zlib
+              "/run/opengl-driver"
+            ];
             packages = with pkgs; [ uv ];
             UV_PYTHON = lib.getExe pkgs.python312;
             TOKENIZERS_PARALLELISM = true;
